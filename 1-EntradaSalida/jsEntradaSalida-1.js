7161b8de77a0e303444225460ce67931aeab2ab7
LeonelAlegre <34493805+LeonelAlegre@users.noreply.github.com>
2020-02-06 11:12:58 -0300

    ejercicio uno

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,13 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	var primerPrecio;
+	var segundoPrecio;
+	var apellido="nadie";
+
+	apellido = "Perez";
+	alert("Esto funciona de maravilla");
+	alert(apellido);
+	alert(primerpri);
 }
 

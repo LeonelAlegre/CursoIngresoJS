9809ae21f3da3978a60a468bed707c5dfce9f586
LeonelAlegre <34493805+LeonelAlegre@users.noreply.github.com>
2020-02-06 11:55:44 -0300

    prompt y console.log

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,15 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+	//prompt devuelve un valor ingresado para ser almacenado
+	nombre = prompt();
+
+	//alert("Bienvenido " + nombre);
+
+	//console.log sirve para mostrar por consola
+	console.log("Bienvenido"+nombre);
+	console.log("Bienvenido "+nombre);
+
 }
 

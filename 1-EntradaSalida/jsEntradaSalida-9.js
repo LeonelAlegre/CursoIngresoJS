5ff8cfe5c0f123b191cc2148ff6119be9e20a80e
LeonelAlegre <34493805+LeonelAlegre@users.noreply.github.com>
2020-02-07 11:52:52 -0300

    porcentaje

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var resultado;
+	var aumento;
+ 
+	aumento = 10;
 
-	
+	sueldo = document.getElementById('sueldo').value;
+	sueldo = parseInt(sueldo);
+
+	resultado = document.getElementById('resultado');
+	resultado = parseInt('resultado');
+
+	aumento = sueldo*aumento/100;
+	resultado = aumento+sueldo;
+	console.log(resultado);
 }

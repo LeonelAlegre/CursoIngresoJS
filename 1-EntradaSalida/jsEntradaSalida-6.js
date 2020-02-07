218f07604ed429e31badec2060acbf6d05e66b15
LeonelAlegre <34493805+LeonelAlegre@users.noreply.github.com>
2020-02-07 10:15:51 -0300

    parseInt()

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,21 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var suma;
 
+	primerNumero =document.getElementById('numeroUno').value;
+	segundoNumero = document.getElementById('numeroDos').value;
+
+	//parseInt() devuelve un valor en formato numerico(numero entero), NO LO MODIFICA
+	// parseInte(primerNumero);
+	//para modificar el valor se lo asigna a una variable.
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+
+	suma = primerNumero+segundoNumero
+
+	alert(suma);
 }
 

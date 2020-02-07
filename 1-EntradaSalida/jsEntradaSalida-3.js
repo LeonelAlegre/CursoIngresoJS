360d9212bee2ee2bdb0231f8dfb4a2f035670a5c
LeonelAlegre <34493805+LeonelAlegre@users.noreply.github.com>
2020-02-07 09:44:42 -0300

    Get Element By ID

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,11 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+
+	//value permite obtener el valor del elemento para ser utilizado
+	nombre = document.getElementById('elNombre').value;
+	alert(nombre);
 }
 
 

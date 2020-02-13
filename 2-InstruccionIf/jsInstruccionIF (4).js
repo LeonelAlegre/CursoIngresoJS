03229b66463156219942351ee884edbb324823a3
alumno <alumno@utnfra.com.ar>
2020-02-13 11:32:38 -0300

    operador logico &

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -4,7 +4,7 @@ function mostrar()
 	var edad
 	edad = document.getElementById('edad').value;
 	edad = parseInt(edad);
-	if(edad > 12 & edad < 18)
+	if(edad > 12 && edad < 18)
 	{
 		alert("Es adoloscente");
 	}

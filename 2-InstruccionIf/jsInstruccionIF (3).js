e35e61a9976241dba82b3ea6994051dd0321410e
alumno <alumno@utnfra.com.ar>
2020-02-13 11:23:35 -0300

    else if

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-//tomo la edad  
-
+	//tomo la edad  
+	var edad
+	edad = document.getElementById('edad').value;
+	edad = parseInt(edad);
+	if(edad > 17)
+	{
+		alert("es mayor de 18");
+	}
+	else
+	{
+		alert("es menor de 18")
+	}
 
 }//FIN DE LA FUNCIÓN

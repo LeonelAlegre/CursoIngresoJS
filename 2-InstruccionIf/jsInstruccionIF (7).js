4f9df202f00c2e4a2ce89ceed162952d03b205c0
unknown <leonel.alegre@hotmail.com>
2020-02-13 20:42:22 -0300

    indistinto de !==

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,19 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	var estado;
+	//ingreso de datos
+	edad = document.getElementById('edad').value;
+	edad = parseInt(edad);
+	estado = document.getElementById('estadoCivil').value ;
+
+	if(edad<18 && estado!== "Soltero")
+	{
+		alert("Es muy pequeño para NO ser soltero");
+	}
 
 	
-
+	console.log("edad: "+edad);
+	console.log("estado: "+estado);
 
 }//FIN DE LA FUNCIÓN

ca41d8310a79524b376e2dc710b5d492805821a9
alumno <alumno@utnfra.com.ar>
2020-02-13 11:04:17 -0300

    if() simple

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
-//tomo la edad  
-
+//tomo la edad
+	//comprueba si el parametro es falso, si no lo es por defecto es verdadero
+	var edad
+	edad = document.getElementById('edad').value;
+	edad = parseInt(edad);
+	if(edad == 15 )
+	{
+		alert("niña bonita");
+	}
 
 
 }//FIN DE LA FUNCIÓN

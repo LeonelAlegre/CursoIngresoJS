4e648361c353f38eb20923aeb0f7c291e19565fa
alumno <alumno@utnfra.com.ar>
2020-02-13 09:31:08 -0300

    Mi primero commit

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,13 +1,6 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	var primerPrecio;
-	var segundoPrecio;
-	var apellido="nadie";
-
-	apellido = "Perez";
-	alert("Esto funciona de maravilla");
-	alert(apellido);
-	alert(primerpri);
+	// testeando git
 }
 

ed3c8e9ff893e956e094ff1fb788f84cfccfbfef
alumno <alumno@utnfra.com.ar>
2020-02-14 09:23:46 -0300

    clasificacion de numeros aleatorios

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,26 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom;
+	numeroRandom = Math.floor(Math.random() * 10) + 1;
+
+	if(numeroRandom == 9 || numeroRandom == 10)
+	{
+		console.log("nota: "+numeroRandom);
+		console.log("EXCELENTE");
+	} 
+	else
+	{
+		if(numeroRandom > 4)
+		{
+			console.log("nota: "+numeroRandom);
+			console.log("APROBÓ");
+		}
+		else
+		{
+			console.log("nota: "+numeroRandom);
+			console.log("Vamos, la proxima se puede");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN

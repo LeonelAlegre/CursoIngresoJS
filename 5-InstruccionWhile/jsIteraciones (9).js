d84dc636546447307456dc77c44bde53cb861b96
unknown <leonel.alegre@hotmail.com>
2020-02-21 16:29:20 -0300

    Clasificacion de valor maximo y minimo. Se agregaron comprovadores para los ingresos numericos y comentarios.

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -11,24 +11,38 @@ function mostrar()
 	numeroIngresado=prompt("Ingresar un numero");
 	numeroIngresado=parseInt(numeroIngresado);
 
+	while(isNaN(numeroIngresado))
+	{	
+		console.log("error");
+		numeroIngresado=prompt("error, ingrese un numero");
+		numeroIngresado=parseInt(numeroIngresado);
+	}
+	//se guarda el unico valor ingresado como numMaximo y Minimo
 	numeroMaximo=numeroIngresado;
 	numeroMinimo=numeroIngresado;
 	
 	console.log("numeroIngresado: "+numeroIngresado);
 	console.log("numeroMaximo: "+numeroMaximo);
 	console.log("numeroMinimo: "+numeroMinimo);
-
+	//se comprueba que el usuario quiera continuar
 	respuesta=prompt("Desea continuar?");
 	while(respuesta=="si")
 	{	
-		
+		//se pide un nuevo numero
 		numeroIngresado=prompt("Ingresar un numero");
 		numeroIngresado=parseInt(numeroIngresado);
-
+		while(isNaN(numeroIngresado))
+		{	
+			console.log("error 2");
+			numeroIngresado=prompt("error, ingrese un numero");
+			numeroIngresado=parseInt(numeroIngresado);
+		}
+		//si es mayor al numero Maximo anterior se sobreescribe dicho valor
 		if(numeroIngresado>numeroMaximo)
 		{
 			numeroMaximo=numeroIngresado;
 		}
+		//si es menor se sobreescribe dicho valor
 		if(numeroIngresado<numeroMinimo)
 		{
 			numeroMinimo=numeroIngresado;
@@ -36,10 +50,10 @@ function mostrar()
 		console.log("numeroIngresado: "+numeroIngresado);
 		console.log("numeroMaximo: "+numeroMaximo);
 		console.log("numeroMinimo: "+numeroMinimo);
-
+		//se comprueba que el usuario quiera continuar
 		respuesta=prompt("Desea continuar?");
 	}
-
+	//muestra los resultados en input tipo texto
 	document.getElementById('maximo').value=numeroMaximo;
 	document.getElementById('minimo').value=numeroMinimo;
 }//FIN DE LA FUNCIÓN

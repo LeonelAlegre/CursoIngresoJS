02f1cdc163842dfbd00d59ba9e455460ece0cc64
alumno <alumno@utnfra.com.ar>
2020-02-21 09:36:41 -0300

    bucle while(), contador y acumulador

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,21 @@
 function mostrar()
 {
+	var contador;
+	var acumulador;
+	var numeroIngresado;
 
-	var contador=0;
-	var acumulador=0;
+	contador=0;
+	acumulador=0;
 
+	while(contador > 5)
+	{
+		contador = contador + 1 //tambien +1 o ++
+		numeroIngresado = pront("ingrese un numero");
+		numeroIngresado = parseInt(numeroIngresado);
+		//el valor del contador se asigna al acumulador
+		acumulador = acumulador+numeroIngresado;
+	}
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
-
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/contador;
 }//FIN DE LA FUNCIÓN

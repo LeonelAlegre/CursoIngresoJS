5a8e5ddd61aa4f3c30ef2628de4dcad1b8a05917
unknown <leonel.alegre@hotmail.com>
2020-02-21 16:07:32 -0300

    Clasificacion de valor maximo y minimo

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -5,22 +5,41 @@ function mostrar()
 	var numeroIngresado;
 	var numeroMaximo;
 	var numeroMinimo;
-	var respuesta='si';
+	var respuesta='no';
 
+	
 	numeroIngresado=prompt("Ingresar un numero");
 	numeroIngresado=parseInt(numeroIngresado);
 
-	while(respuestar=="si")
-	{
-		if()
-		{
+	numeroMaximo=numeroIngresado;
+	numeroMinimo=numeroIngresado;
+	
+	console.log("numeroIngresado: "+numeroIngresado);
+	console.log("numeroMaximo: "+numeroMaximo);
+	console.log("numeroMinimo: "+numeroMinimo);
 
+	respuesta=prompt("Desea continuar?");
+	while(respuesta=="si")
+	{	
+		
+		numeroIngresado=prompt("Ingresar un numero");
+		numeroIngresado=parseInt(numeroIngresado);
+
+		if(numeroIngresado>numeroMaximo)
+		{
+			numeroMaximo=numeroIngresado;
 		}
-		numeroMinimo=numeroIngresado;
-		numeroMaximo=numeroIngresado;
+		if(numeroIngresado<numeroMinimo)
+		{
+			numeroMinimo=numeroIngresado;
+		}
+		console.log("numeroIngresado: "+numeroIngresado);
+		console.log("numeroMaximo: "+numeroMaximo);
+		console.log("numeroMinimo: "+numeroMinimo);
 
 		respuesta=prompt("Desea continuar?");
 	}
 
-
+	document.getElementById('maximo').value=numeroMaximo;
+	document.getElementById('minimo').value=numeroMinimo;
 }//FIN DE LA FUNCIÓN

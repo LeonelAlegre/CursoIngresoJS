31cea01f598c8c86c286eca5fd1f8783b7ae22b2
alumno <alumno@utnfra.com.ar>
2020-02-21 10:18:44 -0300

    negativos y positivos. terminado. +comentarios

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -10,25 +10,30 @@ function mostrar()
 
 	while(respuesta=="si")
 	{
+		//numero ingresado por usuario
 		numeroIngresado = prompt("ingresar un numero");
 		numeroIngresado = parseInt(numeroIngresado);
 
+		//comprobacion para numero positivos +0
 		if(numeroIngresado>0)
-		{
+		{	
+			//el valor de la suma se acumula en la var positivo
 			positivo = positivo + numeroIngresado;
 			console.log("numero ingresado: "+numeroIngresado);
 			console.log("suma: "+positivo);
 		}
 		else
 		{
+			//comprobacion para numeros negativos -0
 			if(numeroIngresado<0)
 			{
+				//el valor del producto se acumula en var negativo
 				negativo = negativo * numeroIngresado;
 				console.log("numero ingresado: "+numeroIngresado);
 				console.log("producto: "+negativo);
 			}
 		}
-
+		//en caso de responder diferente a "si" el bucle acaba
 		respuesta = prompt("desea agregar numeros?");
 	}
 

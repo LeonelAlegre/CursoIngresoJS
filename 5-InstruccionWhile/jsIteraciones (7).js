803e410893495e33fc14c192883f4123decc3535
alumno <alumno@utnfra.com.ar>
2020-02-21 10:25:58 -0300

    isNaN

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,15 +5,21 @@ function mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
-	while(contador == "si")
+	while(respuesta == "si")
 	{
 		contador = contador + 1 //tambien +1 o ++
 		numeroIngresado = prompt("ingrese un numero");
 		numeroIngresado = parseInt(numeroIngresado);
+		//NaN devuelve verdadero mientras no sea un numero
+		while(isNaN(numeroIngresado))
+		{
+			numeroIngresado = prompt("error, ingresar numero");
+			numeroIngresado = parseInt(numeroIngresado);
+		}
 		//el valor del contador se asigna al acumulador
 		acumulador = acumulador+numeroIngresado;
 
-		respuesta = prompt()
+		respuesta = prompt("ingrese si para continuar");
 	}
 
 document.getElementById('suma').value=acumulador;

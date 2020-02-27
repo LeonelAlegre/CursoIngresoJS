0c5e20aeef172e36c326e95e7b31d986b02ae70a
alumno <alumno@utnfra.com.ar>
2020-02-27 11:13:09 -0300

    switch()

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -5,10 +5,41 @@ function mostrar()
 
 	switch(mesDelAño)
 	{
-
+		case "Enero":
+			console.log("el mes es: Enero");
+			break;
+		case "Febrero":
+			console.log("el mes es: Febrero");
+			break;
+		case "Marzo":
+			console.log("el mes es: Marzo");
+			break;
+		case "Abril":
+			console.log("el mes es: Abril");
+			break;
+		case "Mayo":
+			console.log("el mes es: Mayo");
+			break;
+		case "Junio":
+			console.log("el mes es: Junio");
+			break;
+		case "Julio":
+			console.log("el mes es: Julio");
+			break;
+		case "Agosto":
+			console.log("el mes es: Agosto");
+			break;
+		case "Septiembre":
+			console.log("el mes es: Septiembre");
+			break;
+		case "Octubre":
+			console.log("el mes es: Octubre");
+			break;
+		case "Noviembre":
+			console.log("el mes es: Noviembre");
+			break;
+		case "Diciembre":
+			console.log("el mes es: Diciembre");
+			break;
 	}
-
-
-
-
 }//FIN DE LA FUNCIÓN

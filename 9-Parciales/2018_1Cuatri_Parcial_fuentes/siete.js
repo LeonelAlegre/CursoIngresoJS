340d62c9e3f88494e56aa11cd7dcb2ae13e64dc0
alumno <alumno@utnfra.com.ar>
2020-02-27 09:18:18 -0300

    Terminado y optimizado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,7 +1,7 @@
 function mostrar()
 {
 	var notas;
-	var notasTotales=0;
+	var notasTotales;
 	var sexo;
 	var sexoMenor;
 	var promedio;
@@ -9,53 +9,32 @@ function mostrar()
 	var varonesContador;
 	var alumnos; //contador
 
-	alumnos=1;
+	alumnos=0;
 	varonesContador=0;
-	//se guarda la nota del usuario:
-	notas=prompt("ingresar nota");
-	notas=parseInt(notas);
-	//validacion de notas:
-	while(isNaN(notas) || (notas<0 || notas>10))
-	{
-		notas=prompt("Error. Ingresar nota del 0 la 10.");
-		notas=parseInt(notas);
-	}
-
-	sexo=prompt("ingresar sexo (f o m)");
-	//validacion de sexo:
-	while(sexo!="f" && sexo!="m")
-	{
-		sexo=prompt("Error. Ingresar sexo (f o m)");
-	}
-	//se asigna el valor de la nota a nota mayor y menor
-	notaMenor=notas;
-	//
-	sexoMenor=sexo;
-	//notas totales = 0 + nota ingresada
-	notasTotales=notasTotales+notas;
-
-	if(notas>5 && sexo=="m")
-	{
-		varonesContador++; 
-	}
+	notasTotales=0;
 
 	while(alumnos<5)
 	{	
 		notas=prompt("ingresar nota");
 		notas=parseInt(notas);
-		//validacion de notas.
-		while(isNaN(notas) || (notas<0 || notas>10))
+		while(isNaN(notas) || (notas<0 || notas>10))//validacion
 		{
 		
 			notas=prompt("Error. Ingresar nota del 0 la 10.");
 			notas=parseInt(notas);
 		}
+
 		sexo=prompt("ingresar sexo (f o m)");
-		while(sexo!="f" && sexo!="m")
+		while(sexo!="f" && sexo!="m")//validacion
 		{
 			sexo=prompt("Error. Ingresar sexo (f o m)");
 		}
-		notasTotales=notasTotales+notas;
+		if(alumnos==0) //se ejecuta solo la primera vez
+		{
+			notaMenor=notas;
+			sexoMenor=sexo;
+		}
+		
 		if(notas<notaMenor)
 		{
 			notaMenor=notas;
@@ -66,12 +45,23 @@ function mostrar()
 		{
 			varonesContador++; 
 		}
+
+		notasTotales=notasTotales+notas;
 		alumnos++;
+		/*
+		console.log("notas: "+notas);
+		console.log("sexo: "+sexo);
+		console.log("notaMenor: "+notaMenor);
+		console.log("sexoMenor: "+sexoMenor);
+		console.log("varonesContador: "+varonesContador);
+		console.log("notasTotales: "+notasTotales);
+		*/
 	}
 
 	promedio=notasTotales/alumnos;
-
-
-
-
+	//---console.log("promedio: "+promedio);
+	
+	alert("Promedio de las notas: "+promedio);
+	alert("Nota mas baja: "+notaMenor+" sexo: "+sexoMenor);
+	alert("Varones con nota mayor a 6: "+varonesContador);
 }

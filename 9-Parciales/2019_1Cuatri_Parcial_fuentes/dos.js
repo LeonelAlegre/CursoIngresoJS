8c98f9a4f26db549d8d0de188d3ed7dd23f7fcca
alumno <alumno@utnfra.com.ar>
2020-02-28 12:09:38 -0300

    Trabajos de parcial 2/3 resueltos

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,98 @@
 function mostrar()
-{
-  
+{//B)
+	var nombre;
+
+	var cantidadPersonas;
+	var mayorCantPersonas;
+	var huespedMasPersonas;
+
+	var cantidadDias;
+	var personasMasDias;
+	var mayorCantDias;
+	var promedioDias;
+	var totalDias=0;
+	var cantReservas=0;
+
+	var formaPago;
+	var pagoMasUtilizado;
+	var contadorEfectivo=0;
+	var contadorTarjeta=0;
+	var contadorQr=0;
+	
+	var continuar;
+
+
+	do
+	{	
+		do
+		{
+			nombre=prompt("igrese su nombre");
+		}while(!isNaN(nombre));
+		do //------------personas
+		{
+			cantidadPersonas=prompt("Cantidad de personas");
+			cantidadPersonas=parseInt(cantidadPersonas);
+		}while(isNaN(cantidadPersonas)||cantidadPersonas<0);
+		do
+		{
+			mayorCantPersonas=cantidadPersonas;
+			huespedMasPersonas=nombre;
+		}while(cantidadPersonas>mayorCantPersonas);
+
+		do //---------------dias
+		{
+			cantidadDias=prompt("Cantidad de dias");
+			cantidadDias=parseInt(cantidadDias);
+		}while(isNaN(cantidadDias)||cantidadDias<0);
+		do
+		{
+			mayorCantDias=cantidadDias;
+			personasMasDias=cantidadPersonas;
+		}while(cantidadDias>mayorCantDias);
+
+		totalDias=totalDias+cantidadDias;
+
+		do //--------------pago
+		{
+			formaPago=prompt("forma de pago: 'efectivo', 'tarjeta' o 'QR' ");
+		}while(formaPago!="efectivo"&&formaPago!="tarjeta"&&formaPago!="QR");
+		
+		switch(formaPago)
+		{
+			case "efectivo":
+				contadorEfectivo++;
+				break;
+			case "tarjeta":
+				contadorTarjeta++;
+				break;
+			case "QR":
+				contadorQr++;
+				break;
+		}
+		cantReservas++;
+		continuar=prompt("Desea continuar?");
+	}while(continuar!="no");
+
+	if (contadorEfectivo>contadorTarjeta && contadorEfectivo>contadorQr) 
+	{
+		pagoMasUtilizado="efectivo";
+	}
+	else
+	{
+		if(contadorTarjeta>contadorEfectivo && contadorTarjeta>contadorQr)
+		{
+			pagoMasUtilizado="tarjeta";
+		}
+		else
+		{
+			pagoMasUtilizado="QR";
+		}
+	}
+	
+	promedioDias=totalDias/cantReservas;
+
+	document.write("Huespede que trajo mas personas:"+huespedMasPersonas+"<br>");
+	document.write("Cantidad de personas que se quedaron mas dias: "+personasMasDias+"<br>");
+	document.write("Forma de pago mas utilizada: "+pagoMasUtilizado+"<br>");
+	document.write("Promedio de dias por reserva: "+promedioDias+"<br>");
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,89 @@
 
 function mostrar()
 {
+	var nombre;
+
+	var edad;
+	var edadMinima;
+	var edadMaxima;
+	var edadTotal;
+	var edadTotalMujeres;
+	var contadorEdad;
+	var promedioEdad;
+	var promedioMujeres;
+
+	var sexo;
+	var cantidadMujeres;
+
+	var temperatura;
+	var temperaturaJoven;
+	var temperaturaViejo;
+
+	var repeticiones;//contador
+
+	contadorEdad=0;
+	cantidadMujeres=0;
+	edadTotalMujeres=0;
+	contadorEdad=0;
+	edadTotal=0;
+	do
+	{	
+		do
+		{
+			nombre=prompt("igrese su nombre");
+		}while(!isNaN(nombre));
+		//------EDAD
+		do
+		{
+			edad=prompt("Ingresar edad.");
+			edad=parseInt(edad);
+		}while(isNaN(edad));
+
+		contadorEdad++;
+		edadTotal=edad+edadTotal;
+		//-----------------SEXO
+		do
+		{
+			sexo=prompt("ingresar sexo('m' o 'f')");
+		}while(sexo!="m" && sexo!="f");
+		if(sexo=="f")
+		{
+			cantidadMujeres++;
+			edadTotalMujeres=edadTotalMujeres+edad;
+		}
+
+		//-------------TEMPERATURA
+		do
+		{
+			temperatura=prompt("ingresar temperatura corporal");
+			temperatura=parseInt(temperatura);
+		}while(isNaN(temperatura) || temperatura<36);
+		
+		do
+		{
+			edadMinima=edad;
+			temperaturaJoven=temperatura;
+		}while(edad<edadMinima);
+		do
+		{
+			edadMaxima=edad;
+			temperaturaViejo=temperatura;
+		}while(edad>edadMaxima);
+
+		if(temperatura>37 && (edad<14 || edad>59))
+		{
+			alert("riesgo: "+nombre);
+		}
+
+		repeticiones=prompt("desea continuar?");
+	}while(repeticiones!="no");
+
+	promedioEdad=edadTotal/contadorEdad;
+	promedioMujeres=edadTotalMujeres/cantidadMujeres;
+
+	document.write("temperatura del mas joven: "+temperaturaJoven+"<br>");
+	document.write("temperatura del mas viejo: "+temperaturaViejo+"<br>");
+	document.write("promedio de edad total: "+promedioEdad+"<br>");
+	document.write("promedio de edad de mujeres: "+promedioMujeres+"<br>");
 
 }

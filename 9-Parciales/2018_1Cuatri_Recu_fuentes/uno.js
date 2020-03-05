f19a3a2c5c358f66fe895bc7b991df26f657e595
unknown <leonel.alegre@hotmail.com>
2020-03-05 17:20:11 -0300

    ejercicio de parcial resuelto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,27 @@
 
 function mostrar()
 {
+	var anchoRectangulo;
+	var largoRectangulo;
+	var perimetro;
 
+	do
+	{
+		anchoRectangulo=prompt("ingrese el ancho del rectangulo");
+		anchoRectangulo=parseInt(anchoRectangulo);
+	}while(isNaN(anchoRectangulo)||anchoRectangulo<1);
+
+	do
+	{
+		largoRectangulo=prompt("ingrese el largo del rectangulo");
+		largoRectangulo=parseInt(largoRectangulo);
+	}while(isNaN(largoRectangulo)||largoRectangulo<1);
+
+
+
+	perimetro=anchoRectangulo*2+largoRectangulo*2;
+
+	console.log("largo: "+largoRectangulo);
+	console.log("ancho: "+anchoRectangulo);
+	alert("El perimetro del rectangulo es: "+perimetro);
 }

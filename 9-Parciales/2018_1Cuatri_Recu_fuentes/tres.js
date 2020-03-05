27b20b274141bb10161d5eb7d5274c35c3ed38e2
unknown <leonel.alegre@hotmail.com>
2020-03-05 17:36:45 -0300

    ejercicio 3 de parcial resuelto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var total;
 
+	do
+	{
+		precio=prompt("ingresar el precio");
+	}while(isNaN(precio));
+
+	do
+	{
+		porcentaje=prompt("ingresar el porcentaje de descuento");
+	}while(isNaN(porcentaje));
+
+	total=precio-precio*porcentaje/100;
+
+	document.getElementById('elPrecioFinal').value=total;
 }

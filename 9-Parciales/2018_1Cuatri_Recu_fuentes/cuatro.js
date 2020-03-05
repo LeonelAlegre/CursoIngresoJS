86978edf98bddcd2cc54a645c6d91919e042b517
unknown <leonel.alegre@hotmail.com>
2020-03-05 17:59:33 -0300

    ejercicio 4 de parcial resuelto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,44 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
 
+	do
+	{
+		numeroUno=prompt("ingresar el primer numero");
+		numeroUno=parseInt(numeroUno);
+	}while(isNaN(numeroUno));
+
+	do
+	{
+		numeroDos=prompt("ingresar el segundo numero");
+		numeroDos=parseInt(numeroDos);
+	}while(isNaN(numeroDos));
+
+	suma= numeroUno+numeroDos;
+
+	if(numeroUno==numeroDos)
+	{
+		numeroUno=numeroUno.toString();
+		numeroDos=numeroDos.toString();
+
+		alert(numeroUno+numeroDos);
+	}
+	else 
+	{
+		if(numeroUno>numeroDos)
+		{
+			alert(numeroUno/numeroDos);
+		}
+		else
+		{
+			alert(suma);
+		}
+	}
+
+	if(suma<50)
+	{
+		alert("la suma es "+suma+" y es menor a 50");
+	}
 }

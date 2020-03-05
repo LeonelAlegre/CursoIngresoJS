8e6052f8dfe63c61414472f1cc9c79ff7b905303
unknown <leonel.alegre@hotmail.com>
2020-03-05 18:10:43 -0300

    ejercicio 5 de parcial resuelto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
+	var dia;
 
+	dia=prompt("ingresar dia de la semana");
+
+	switch(dia)
+	{
+		case "sabado":
+			alert("buen finde");
+			break;
+		case "domingo":
+			alert("buen finde");
+			break;	
+		case "lunes":
+			alert("a trabajar");
+			break;
+		case "martes":
+			alert("a trabajar");
+			break;
+		case "miercoles":
+			alert("a trabajar");
+			break;
+		case "jueves":
+			alert("a trabajar");
+			break;
+		case "viernes":
+			alert("a trabajar");
+			break;			
+		default:
+			alert("dia no valido");
+	}
 }
